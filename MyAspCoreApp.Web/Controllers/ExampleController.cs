781734919dc6304b886dc26a3ac48382189ca472
unknown <atilla.kalayy@gmail.com>
2023-01-17 20:55:18 +0300

    Action Results lesson completed.

---
+++ MyAspCoreApp.Web/Controllers/ExampleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspCoreApp.Web.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ContentResult()
+        {
+            return Content("ContentResult");
+        }
+
+        public IActionResult JsonResult()
+        {
+            return Json(new { Id = 1, name = "Kalem 1", price = 17.5 });
+        }
+
+        public IActionResult EmptyResult()
+        {
+            return new EmptyResult();
+        }
+    }
+}
