36e4a86f4b0dcbacebab0bed185f78db2ba7c1f2
unknown <atilla.kalayy@gmail.com>
2023-02-04 15:50:58 +0300

    Form Elements Datetime Lesson completed.

--- MyAspCoreApp.Web/Models/Product.cs
+++ MyAspCoreApp.Web/Models/Product.cs
@@ -8,6 +8,7 @@
         public int Stock { get; set; }
         public string Description { get; set; }
         public string? Color { get; set; }
+        public DateTime? PublishDate { get; set; }
         public bool IsPublished { get; set; }
         public string Expire { get; set; }
     }
