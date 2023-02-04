32fb7b6b7a8bd5579b8405aebcf80b0d8419ed88
unknown <atilla.kalayy@gmail.com>
2023-02-04 16:23:00 +0300

    The Form Elements Update.cshtml tutorial is complete.

--- MyAspCoreApp.Web/Controllers/PoductsController.cs
+++ MyAspCoreApp.Web/Controllers/PoductsController.cs
@@ -73,6 +73,19 @@ namespace MyAspCoreApp.Web.Controllers
         public IActionResult Update(int id)
         {
             var product = _dbContext.Products.Find(id);
+
+            ViewBag.ExpireValue = Convert.ToInt32(product.Expire);
+
+            ViewBag.Expire = new Dictionary<string, int>() { { "1 Ay", 1 }, { "3 Ay", 3 }, { "6 Ay", 6 }, { "12 Ay", 12 } };
+
+            ViewBag.ColorSelect = new SelectList(new List<ColorSelectList>()
+            {
+                new(){Data = "Mavi",Value = "Mavi"},
+                new(){Data = "Kırmızı",Value = "Kırmızı"},
+                new(){Data = "Beyaz",Value = "Beyaz"},
+                new(){Data = "Siyah",Value = "Siyah"}
+            }, "Value", "Data", product.Color);
+
             return View(product);
         }
         [HttpPost]
