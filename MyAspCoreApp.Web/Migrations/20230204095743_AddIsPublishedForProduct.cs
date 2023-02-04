3746c5e67eca4c3c2fce960a164021620d67a7a5
unknown <atilla.kalayy@gmail.com>
2023-02-04 13:02:42 +0300

    Form Elements Input-Checkbox Lesson Completed.

---
+++ MyAspCoreApp.Web/Migrations/20230204095743_AddIsPublishedForProduct.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyAspCoreApp.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsPublishedForProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublished",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPublished",
+                table: "Products");
+        }
+    }
+}
--- MyAspCoreApp.Web/Models/Product.cs
+++ MyAspCoreApp.Web/Models/Product.cs
@@ -7,5 +7,6 @@
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public string? Color { get; set; }
+        public bool IsPublished { get; set; }
     }
 }
