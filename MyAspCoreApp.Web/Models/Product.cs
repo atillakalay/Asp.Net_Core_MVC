507d3940e6ca8349932a45083a7322781b47ee80
unknown <atilla.kalayy@gmail.com>
2023-02-04 15:17:09 +0300

    Form Elements Textarea Lesson completed.

--- MyAspCoreApp.Web/Models/Product.cs
+++ MyAspCoreApp.Web/Models/Product.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
+        public string Description { get; set; }
         public string? Color { get; set; }
         public bool IsPublished { get; set; }
         public string Expire { get; set; }
