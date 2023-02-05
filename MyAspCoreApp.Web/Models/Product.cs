7cd7b70537ba1f642c9a9c270a049f32ffd09889
unknown <atilla.kalayy@gmail.com>
2023-02-05 15:09:57 +0300

    The Model/Form Validation Required-2 course has been completed.

--- MyAspCoreApp.Web/Models/Product.cs
+++ MyAspCoreApp.Web/Models/Product.cs
@@ -1,15 +1,18 @@
-﻿using Microsoft.Build.Framework;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MyAspCoreApp.Web.Models
 {
     public class Product
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "İsim alanı boş olamaz")]
         public string Name { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
-        public string Description { get; set; }
+        [Required(ErrorMessage = "Fiyat alanı boş olamaz")]
+        public decimal? Price { get; set; }
+        [Required(ErrorMessage = "Stok alanı boş olamaz")]
+        public int? Stock { get; set; }
+        [Required(ErrorMessage = "Açıklama alanı boş olamaz")]
+        public string? Description { get; set; }
         public string? Color { get; set; }
         public DateTime? PublishDate { get; set; }
         public bool IsPublished { get; set; }
