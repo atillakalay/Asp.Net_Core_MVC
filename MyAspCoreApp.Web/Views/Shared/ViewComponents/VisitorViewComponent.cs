0d0732547f0bc16d8b88f46d84ba30016df9c33a
unknown <atilla.kalayy@gmail.com>
2023-02-11 00:32:34 +0300

    View Component Example: Visitor component-5 course has been completed.

--- MyAspCoreApp.Web/Views/Shared/ViewComponents/VisitorViewComponent.cs
+++ MyAspCoreApp.Web/Views/Shared/ViewComponents/VisitorViewComponent.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyAspCoreApp.Web.Models;
+using MyAspCoreApp.Web.ViewModels;
 
 namespace MyAspCoreApp.Web.Views.Shared.ViewComponents
 {
     public class VisitorViewComponent : ViewComponent
     {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public VisitorViewComponent(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            var visitors = await _context.Visitors.ToListAsync();
+            var visitorViewModels = _mapper.Map<List<VisitorViewModel>>(visitors);
+            ViewBag.visitorViewModel = visitorViewModels;
             return View();
         }
     }
