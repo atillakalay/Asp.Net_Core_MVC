94a6c12466aa202cc7cad914a7100c71874155ee
unknown <atilla.kalayy@gmail.com>
2023-02-14 11:57:54 +0300

    Tag Helper Example 2 :<product-show> completed.

---
+++ MyAspCoreApp.Web/TagHelpers/ItalicTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MyAspCoreApp.Web.TagHelpers
+{
+    public class ItalicTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.PreContent.SetHtmlContent("<i>");
+
+            output.PostContent.SetHtmlContent("</li>");
+        }
+    }
+}
---
+++ MyAspCoreApp.Web/TagHelpers/ProductShowTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using MyAspCoreApp.Web.Models;
+
+namespace MyAspCoreApp.Web.TagHelpers
+{
+    public class ProductShowTagHelper : TagHelper
+    {
+        public Product Product { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Content.SetHtmlContent(@$"<ul class='list-group'>
+<li class='list-group-item'>{Product.Id}</li>
+<li class='list-group-item'>{Product.Name}</li>
+<li class='list-group-item'>{Product.Price}</li>
+<li class='list-group-item'>{Product.Stock}</li>
+</ul>");
+        }
+    }
+}
