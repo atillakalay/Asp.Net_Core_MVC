e0c3178a002225155c5874a9e1c5f5a6a8498a3c
unknown <atilla.kalayy@gmail.com>
2023-02-14 12:47:35 +0300

    Tag Helper Example 1 :<image-thumbnail> Part 2 is complete.

---
+++ MyAspCoreApp.Web/TagHelpers/ImageThumbnail.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MyAspCoreApp.Web.TagHelpers
+{
+    public class ImageThumbnail : TagHelper
+    {
+        public string ImageSrc { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "img";
+            string fileName = ImageSrc.Split(".")[0];
+            string fileExtension = Path.GetExtension(ImageSrc);
+
+            output.Attributes.SetAttribute("src", $"{fileName}-100X100{fileExtension}");
+        }
+    }
+}
