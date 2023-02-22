9ff4480408fa3bd5292c9b6a0c8d559f1cf90067
unknown <atilla.kalayy@gmail.com>
2023-02-22 14:29:17 +0300

    The NotFoundFilter Sample Scenario-1 lesson has been completed.

---
+++ MyAspCoreApp.Web/Filters/NotFoundFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyAspCoreApp.Web.Models;
+
+namespace MyAspCoreApp.Web.Filters
+{
+    public class NotFoundFilter : ActionFilterAttribute
+    {
+        private readonly AppDbContext _context;
+
+        public NotFoundFilter(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var idValue = context.ActionArguments.Values.First();
+            var id = (int)idValue;
+
+            var hasProducts = _context.Products.Any(x => x.Id == id);
+            if (hasProducts == false)
+            {
+                context.Result = new RedirectToActionResult("Error", "Home", null);
+            }
+        }
+    }
+}
