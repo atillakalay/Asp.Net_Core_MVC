8c2149f80a8d067016f27b998dd771f4088fd90f
unknown <atilla.kalayy@gmail.com>
2023-02-23 15:33:27 +0300

    Middleware Sample Scenario-2 Part 2 course has been completed.

--- MiddlewareExample.Web/Middlewares/WhiteIpAddressControlMiddleware.cs
+++ MiddlewareExample.Web/Middlewares/WhiteIpAddressControlMiddleware.cs
@@ -1,6 +1,31 @@
-﻿namespace MiddlewareExample.Web.Middlewares
+﻿using System.Net;
+
+namespace MiddlewareExample.Web.Middlewares
 {
-    public class WhiteIpAdressControlMiddleware
+    public class WhiteIpAddressControlMiddleware
     {
+        private readonly RequestDelegate _requestDelegate;
+        private const string WhiteIpAddress = "::1";
+
+        public WhiteIpAddressControlMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var requestIpAddress = context.Connection.RemoteIpAddress;
+            bool anyWhiteIpAddress = IPAddress.Parse(WhiteIpAddress).Equals(requestIpAddress);
+
+            if (anyWhiteIpAddress)
+            {
+                await _requestDelegate(context);
+            }
+            else
+            {
+                context.Response.StatusCode = HttpStatusCode.Forbidden.GetHashCode();
+                await context.Response.WriteAsync("Forbidden");
+            }
+        }
     }
 }
