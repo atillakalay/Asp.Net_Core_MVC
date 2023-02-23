8dbdf422a9d3cee15f31298f386787af48b4e4a1
unknown <atilla.kalayy@gmail.com>
2023-02-23 15:12:44 +0300

    The Creating Middleware with the MapWhen() Method lesson has been completed.

--- MiddlewareExample.Web/Program.cs
+++ MiddlewareExample.Web/Program.cs
@@ -16,6 +16,24 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+#region Mapwhen Kullanýmý
+//app.MapWhen(context => context.Request.Query.ContainsKey("name"), app =>
+//{
+//    app.Use(async (context, next) =>
+//    {
+//        await context.Response.WriteAsync("Before 1. Middleware\n");
+
+//        await next();
+
+//        await context.Response.WriteAsync("After 1. Middleware");
+//    });
+//    app.Run(async context =>
+//    {
+//        await context.Response.WriteAsync("Terminal 3. Middleware\n");
+//    });
+//}); 
+#endregion
+
 
 #region Use Ve Run Kullanýmý
 //app.Use(async (context, next) =>
@@ -45,13 +63,13 @@ app.UseStaticFiles();
 
 #region Map ve Run Kullanýmý
 
-app.Map("/ornek", app =>
-{
-    app.Run(async context =>
-    {
-        await context.Response.WriteAsync("Ornek url icin middleware.");
-    });
-});
+//app.Map("/ornek", app =>
+//{
+//    app.Run(async context =>
+//    {
+//        await context.Response.WriteAsync("Ornek url icin middleware.");
+//    });
+//});
 
 #endregion
 
