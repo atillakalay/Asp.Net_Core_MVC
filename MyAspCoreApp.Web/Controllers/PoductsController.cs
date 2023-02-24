8ed34c9a082ef72ee4e680a3fc1e6b550a1f45b7
unknown <atilla.kalayy@gmail.com>
2023-02-24 12:07:27 +0300

    File Upload Part 7 lesson has been completed.

--- MyAspCoreApp.Web/Controllers/PoductsController.cs
+++ MyAspCoreApp.Web/Controllers/PoductsController.cs
@@ -107,18 +107,25 @@ namespace MyAspCoreApp.Web.Controllers
             {
                 try
                 {
-                    var root = _fileProvider.GetDirectoryContents("wwwroot");
-                    var images = root.First(x => x.Name == "images");
-
-                    var randomImageName = Guid.NewGuid() + Path.GetExtension(productViewModel.Image.FileName);
-
-
-                    var path = Path.Combine(images.PhysicalPath, randomImageName);
-                    using var stream = new FileStream(path, FileMode.Create);
-                    productViewModel.Image.CopyTo(stream);
-
                     var product = _mapper.Map<Product>(productViewModel);
-                    product.ImagePath = randomImageName;
+                    if (productViewModel.Image != null & productViewModel.Image.Length > 0)
+                    {
+                        var root = _fileProvider.GetDirectoryContents("wwwroot");
+                        var images = root.First(x => x.Name == "images");
+
+                        var randomImageName = Guid.NewGuid() + Path.GetExtension(productViewModel.Image.FileName);
+
+
+                        var path = Path.Combine(images.PhysicalPath, randomImageName);
+                        using var stream = new FileStream(path, FileMode.Create);
+                        productViewModel.Image.CopyTo(stream);
+
+                        product.ImagePath = randomImageName;
+                    }
+
+
+
+
 
                     _dbContext.Products.Add(product);
                     _dbContext.SaveChanges();
