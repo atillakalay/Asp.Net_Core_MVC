1bc29c9031a8ae5f17e7c87a01bd50569fce0eea
unknown <atilla.kalayy@gmail.com>
2023-02-25 14:09:01 +0300

    Cookie lesson completed.

---
+++ MyAspCoreApp.Web/Controllers/CookieController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspCoreApp.Web.Controllers
+{
+    public class CookieController : Controller
+    {
+        public IActionResult CookieCreate()
+        {
+            HttpContext.Response.Cookies.Append("backgorund-color", "red", new CookieOptions()
+            {
+                Expires = DateTime.Now.AddDays(2)
+            });
+            return View();
+        }
+
+        public IActionResult CookieRead()
+        {
+            var bgColor = HttpContext.Request.Cookies["backgorund-color"];
+            ViewBag.bgColor = bgColor;
+            return View();
+        }
+    }
+}
