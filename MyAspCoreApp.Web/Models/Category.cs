d6fe06ff8b4a7514d12281a24021c7564c1644ee
unknown <atilla.kalayy@gmail.com>
2023-02-25 13:07:05 +0300

    Category Controller Test Process lesson completed.

--- MyAspCoreApp.Web/Models/Category.cs
+++ MyAspCoreApp.Web/Models/Category.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public List<Product> Products { get; set; }
+
+        public List<Product>? Products { get; set; }
     }
 }
