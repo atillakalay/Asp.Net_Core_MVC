c1a114c0df6e0af7c7bb77bc4ab72dcbe73dde98
unknown <atilla.kalayy@gmail.com>
2023-02-25 14:31:06 +0300

    The Creating Ajax Action Methods lesson has been completed.

---
+++ MyAspCoreApp.Web/Controllers/VisitorAjaxController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyAspCoreApp.Web.Models;
+using MyAspCoreApp.Web.ViewModels;
+
+namespace MyAspCoreApp.Web.Controllers
+{
+    public class VisitorAjaxController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly AppDbContext _appDbContext;
+
+        public VisitorAjaxController(IMapper mapper, AppDbContext appDbContext)
+        {
+            _mapper = mapper;
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SaveVisitorComment(VisitorViewModel visitorViewModel)
+        {
+
+            var visitor = _mapper.Map<Visitor>(visitorViewModel);
+            visitor.Created = DateTime.Now;
+            _appDbContext.Visitors.Add(visitor);
+            _appDbContext.SaveChanges();
+
+            return Json(new { IsSuccess = true });
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> VisitorCommentList()
+        {
+            var visitors = await _appDbContext.Visitors.ToListAsync();
+            var visitorViewModels = _mapper.Map<List<VisitorViewModel>>(visitors);
+            return Json(visitorViewModels);
+        }
+    }
+
+
+}
